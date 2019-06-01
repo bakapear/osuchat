2aec096667d5e8e5e292ab8c7555ba049075b302
bakapear <proxybomb@gmail.com>
2019-06-01 22:10:36 +0200

    init

---
+++ index.js
@@ -0,0 +1,29 @@
+let irc = require('./irc')
+let got = require('got')
+let url = process.env.HOOK
+
+let client = new irc.Client('irc.ppy.sh', process.env.NAME, {
+  channels: ['#osu'], port: '6667', password: process.env.PASS
+})
+
+client.addListener('message', function (from, to, message) {
+  post(from, message)
+})
+
+function post (name, message) {
+  try {
+    got.post(url, {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: {
+        'username': name,
+        'avatar_url': 'https://i.imgur.com/SYlWC1n.png',
+        'content': message
+      },
+      json: true
+    })
+  } catch (e) { console.log('POST - ' + e) }
+}
+
+process.on('unhandledRejection', () => { console.log('yep') })
